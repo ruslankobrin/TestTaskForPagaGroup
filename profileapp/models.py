49026adfd6bfa9ccaaee6dcbb255fbbccc07cb86
ruslankobrin <ruslankobrin@gmail.com>
2020-05-10 19:40:40 +0300

    Created model Profile.

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
