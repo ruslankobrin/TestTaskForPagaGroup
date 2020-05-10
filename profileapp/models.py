772275c1f3d43bd404f30ab5c06aee1dc25173cc
ruslankobrin <ruslankobrin@gmail.com>
2020-05-11 02:44:33 +0300

    add field to Profile model

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,5 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=40, null=True, blank=True)
     location = models.CharField(max_length=30, blank=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
     birth_date = models.DateField(null=True, blank=True)
